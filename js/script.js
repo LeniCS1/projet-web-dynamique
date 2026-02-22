3d19d404ee5204532ba74eec1e72f22bbadccb66
Gabriel CHAPELON <gabriel.chapelon@efrei.net>
2026-02-22 12:53:06 +0100

    ajout de java

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.querySelector('.efierte-insta').addEventListener('click', function(e) {
+    e.preventDefault();
+    alert("Vous allez être redirigé vers instagram");
+    window.open('https://www.instagram.com/efierte_efrei/', '_blank');
+});
